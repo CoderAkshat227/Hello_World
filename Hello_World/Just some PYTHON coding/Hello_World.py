c1973862ddb422995b702a778d381e46b2ed2d43
BruhakshatYT <akshatplays@gmail.com>
2020-08-25 10:42:46 +0800

    Organzed Files, Updated File Name, Started Python Sections

---
+++ Hello_World/Just some PYTHON coding/Hello_World.py
@@ -0,0 +1,7 @@
+print("Hi! My name is Akshat, and this is PYTHON! It's pretty cool!)
+print("Here is a small quiz!");
+
+question_prompts [
+"What colour are apples?/n (a)Green (b) Turqouise (c) Red (d)Pink"
+
+]
